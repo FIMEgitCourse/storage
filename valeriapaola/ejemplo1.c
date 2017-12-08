7bf5954d0926b2c340ae9c77e409f3fe80fd7793
Valeria Paola <valeria.gonzalezdn@uanl.edu.mx>
2017-12-08 09:18:02 -0600

    agregue el archivo ejercicio1

---
+++ valeriapaola/ejemplo1.c
@@ -0,0 +1,33 @@
+//EJEMPLO No. 1: PROGRAMA QUE LEE DOS NÚMEROS Y ESCRIBE EL MAYOR DE LOS DOS.
+
+#include <stdio.h>
+
+main()
+
+{
+
+int x, y;
+
+printf(“Escribe el primer número: “);
+
+scanf(“%d”,&x);
+
+printf(“Escribe el segundo número:”);
+
+scanf(“%d”,&y);
+
+if (x > y)
+
+printf(“El mayor es: %d”,x);
+
+else
+
+if ( y > x )
+
+printf(“El mayor es: %d”,y);
+
+else
+
+printf(“Son iguales”);
+
+}
